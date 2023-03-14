7d9831ba427272ee98466073e136cfad79224425
metaOkami <91599092+Dmm00@users.noreply.github.com>
2023-03-14 16:50:26 +0100

    Commit final

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -2,28 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//En este script determinamos las funciones por defecto que van a tener cada command
+//y los distintos commands que hay.
+
 public abstract class Command 
 {
+    //Variable protegida de la distancia que ha de moverse el cubo
     protected float moveDistance = 1f;
+    //Funcion con obligacion de hacer override en cada command de ejecutar
     public abstract void Execute(Transform objectToMove, Command commandDone);
-
+    //Funcion para deshacer el ejecutar
     public virtual void Undo(Transform objectToMove) { }
+    //Funcion auxiliar para mover el cubo en una direccion concreta
     public virtual void Move(Transform objectToMove) { }
 
 }
 
+//Command de mover hacia alante
 public class MoveForward : Command
 {
     //Funcion específica dentro de este comando que se produce al ejecutar el mismo
     public override void Execute(Transform boxTransform, Command command)
     {
-        Move(boxTransform);
-        CubeBehaviour.OldActions.Add(command);
+        //Funcion aun en pruebas, antes de moverlo comprobamos que no haya límites en la direccion a la que vamos a moverlo
+        if(!Physics.Raycast(boxTransform.position,boxTransform.forward+new Vector3(0,1,0),out RaycastHit hit, 1.05f)){
+            Move(boxTransform);
+            CubeBehaviour.OldActions.Add(command);
+            Debug.Log("NO Detecto limites");
+        }
+        else
+        {
+            Debug.Log("Detecto limites");
+        }
+        
     }
 
     //Funcion específica dentro de este comando que deshace la función de ejecutar del mismo
     public override void Undo(Transform objectToMove)
     {
+        //Para deshacer, simplemente hacemos la función inversa a la hecha en el execute, aqui no comprobamos
+        //limites debido a que ya se ha mirando anteriormente.
         objectToMove.Translate(-objectToMove.forward * moveDistance,Space.World);
     }
 
@@ -33,14 +51,17 @@ public class MoveForward : Command
         objectToMove.Translate(objectToMove.forward * moveDistance, Space.World);
     }
 }
-
+//Command de mover hacia la izquierda
 public class MoveLeft : Command
 {
     //Funcion específica dentro de este comando que se produce al ejecutar el mismo
     public override void Execute(Transform boxTransform, Command command)
     {
-        Move(boxTransform);
-        CubeBehaviour.OldActions.Add(command);
+        if(!Physics.Raycast(boxTransform.position, -boxTransform.right + new Vector3(0, 1, 0), out RaycastHit hit, 1.05f))
+        {
+            Move(boxTransform);
+            CubeBehaviour.OldActions.Add(command);
+        }
     }
 
     //Funcion específica dentro de este comando que deshace la función de ejecutar del mismo
@@ -55,14 +76,22 @@ public class MoveLeft : Command
         objectToMove.Translate(-objectToMove.right * moveDistance,Space.World);
     }
 }
-
+//Command de mover a la derecha
 public class MoveRight : Command
 {
     //Funcion específica dentro de este comando que se produce al ejecutar el mismo
     public override void Execute(Transform boxTransform, Command command)
     {
-        Move(boxTransform);
-        CubeBehaviour.OldActions.Add(command);
+        if(!Physics.Raycast(boxTransform.position, boxTransform.right + new Vector3(0, 1, 0), out RaycastHit hit, 1.05f))
+        {
+            Move(boxTransform);
+            CubeBehaviour.OldActions.Add(command);
+        }
+        else
+        {
+            Debug.Log("Detecto limites");
+        }
+        
     }
 
     //Funcion específica dentro de este comando que deshace la función de ejecutar del mismo
@@ -78,6 +107,7 @@ public class MoveRight : Command
     }
 }
 
+//Command que deshace la ultima accion hecha
 public class UndoCommand : Command
 {
     public override void Execute(Transform objectToMove, Command commandDone)
--- Assets/Scripts/CubeBehaviour.cs
+++ Assets/Scripts/CubeBehaviour.cs
@@ -6,20 +6,27 @@ public class CubeBehaviour : MonoBehaviour
 {
     //Variable de tipo cubeBehaviour a la que acceder desde otras clases en caso de ser necesario
     public static CubeBehaviour cubeInstance { get; private set; }
-    public static bool canMove = false;
-
+    //Variable que determina si el cubo se puede mover o no
+    public bool canMove = false;
+    //Variables tipo command para especificar que ejecuta cada botón especifico
     private Command ButtonW, ButtonD, ButtonA, ButtonZ;
+    //Numero de oportunidades que tiene el jugador para deshacer
     private int undoChances = 3;
+    //Variable para no poder mover el cubo mas de una vez de seguido
     public int moveChances = 1;
+    //Lista de acciones que se hace para la hora de poder hacer una replay de cara a futuro y la funcion de deshacer
     public static List<Command> OldActions = new List<Command>();
-
+    //Canvas de victoria
     public GameObject winCanvas;
+    //Variable que determina el objeto que va a ser observado
     Observable cubeObservable = new Observable();
     //Para mantener la instancia del cubo intacta nos aseguramos preguntando si es nula y diferente a este script
     //Este metodo de instanciar clases es el conocido como Singleton
     public void Awake()
     {
+        //Nos aseguramos de que el canvas de victoria se inicie estando desactivado
         winCanvas.SetActive(false);
+        //Hago un singleton para asegurarme que no se instancie esta clase mas de una vez.
         if (cubeInstance!=null && cubeInstance != this)
         {
             Destroy(this);
@@ -29,22 +36,25 @@ public class CubeBehaviour : MonoBehaviour
         {
             cubeInstance = this;
         }
-    }//Aqui hago un singleton para poder impelentarlo en futuros niveles y queremos acceder a una funcionalidad o variable del cubo
+    }
 
     private void Start()
     {
+        //Declaro la funcionalidad de cada boton
         ButtonW = new MoveForward();
         ButtonA = new MoveLeft();
         ButtonD = new MoveRight();
         ButtonZ = new UndoCommand();
 
+        //Objeto de tipo EndCube a la que pasamos como parámetro el canvas que ha de activarse cuando se le lance el evento
         EndCube endCube = new EndCube(winCanvas);
+        //Añadimos como observador al cubo para que observe este objeto y reaccione a los cambios oportunos
         cubeObservable.AddObserver(endCube);
     }
 
     private void Update()
     {
-        Debug.Log(canMove);
+        //Si el cubo se puede mover, recibimos los inputs para moverlo.
         if (canMove)
         {
             HandleInput();
@@ -53,12 +63,17 @@ public class CubeBehaviour : MonoBehaviour
 
     public void HandleInput()
     {
+        //En esta funcion comprobamos si se está presionando una tecla de las que acciona un cambio, accedemos a su command y lo ejecutamos
+        //en caso de que la variable moveChances sea 0.
+        //Cuando pulsamos el boton, se restan los moveChances, ejecuta la funcion deseada, pone en false el canMove y reestablece el movimiento
+        //al jugador.
         if (Input.GetKeyDown(KeyCode.W))
         {
+            
             if (moveChances > 0)
             {
                 moveChances--;
-                ButtonW.Execute(cubeInstance.transform, ButtonW);
+                ButtonW.Execute(transform, ButtonW);
                 canMove = false;
                 PlayerController.instance.gameObject.GetComponent<CharacterController>().enabled = true;
             }
@@ -68,17 +83,16 @@ public class CubeBehaviour : MonoBehaviour
             if (moveChances > 0)
             {
                 moveChances--;
-                ButtonD.Execute(cubeInstance.transform, ButtonD);
+                ButtonD.Execute(transform, ButtonD);
                 canMove = false;
                 PlayerController.instance.gameObject.GetComponent<CharacterController>().enabled = true;
-            }
-        }
+            }        }
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (moveChances > 0)
             {
                 moveChances--;
-                ButtonA.Execute(cubeInstance.transform, ButtonA);
+                ButtonA.Execute(transform, ButtonA);
                 canMove = false;
                 PlayerController.instance.gameObject.GetComponent<CharacterController>().enabled = true;
             }
@@ -88,7 +102,7 @@ public class CubeBehaviour : MonoBehaviour
             if (undoChances > 0)
             {
                 undoChances--;
-                ButtonZ.Execute(cubeInstance.transform, ButtonZ);
+                ButtonZ.Execute(transform, ButtonZ);
                 canMove = false;
                 PlayerController.instance.gameObject.GetComponent<CharacterController>().enabled = true;
             }
@@ -97,12 +111,29 @@ public class CubeBehaviour : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        //En caso de que se entre en la posición deseada (con un cubo como preview para el jugador y referencia para nosotros) notificamos a nuestro
+        //observador de que estamos donde tenemos que estar.
         if (other.tag == "FinalCube")
         {
             if (transform.position == other.transform.position)
             {
                 cubeObservable.Notify();
             }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        try
+        {
+
+            Gizmos.DrawLine(transform.position, (transform.forward * 1.05f) + new Vector3(0, 1, 0) );
+        }
+        catch (System.Exception)
+        {
+
+            
         }
     }
 
